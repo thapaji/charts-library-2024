d86128dbf81cbee861342c9a37cf449ef9c24ab5
Sujan <mail.thapasujan@gmail.com>
2024-04-01 14:59:38 +1100

    bar graph v1 implemented

--- script.js
+++ script.js
@@ -2,25 +2,19 @@ const taskList = [];
 const entryTable = document.getElementById('entryList');
 const alertElm = document.getElementsByClassName('alert-hour')[0];
 const savedElm = document.getElementById('savedElm');
-const twklHr = 175;
 
 const handleSubmit = (form) => {
     const newForm = new FormData(form);
-    const task = newForm.get('label');
-    const hour = +newForm.get('data');
+    const label = newForm.get('label');
+    const data = +newForm.get('data');
 
     const obj = {
-        task,
-        hour,
+        label,
+        data,
     }
     console.log(obj);
-    const prevTotal = displayTotal();
-    if ((prevTotal + hour) > twklHr) {
-        return alert('Sorry you do not have enough time left!!!!');
-    }
     taskList.push(obj);
     display();
-    displayTotal();
     form.reset();
 }
 
@@ -31,8 +25,8 @@ const display = () => {
         strEntry += `
     <tr>
     <td>${i}</td>
-    <td>${item.task}</td>
-    <td>${item.hour}hrs</td>
+    <td>${item.label}</td>
+    <td>${item.data}hrs</td>
     <td class="text-end">
       <button class="btn btn-danger btn-sm" onClick="handOnDelete(${i})"><i class="fa-solid fa-trash"></i></button>
     </td>
@@ -43,22 +37,50 @@ const display = () => {
 
     // alertElm.innerText = `Total hours allocated = ${total()}hrs`;
     entryTable.innerHTML = strEntry;
+    createBarGraph(taskList);
 }
 
-const displayTotal = () => {
-    const total = taskList.reduce((subTotal, item) => {
-        return subTotal + item.hour;
-    }, 0);
-    alertElm.innerText = `Total hours allocated = ${total}hrs`;
-    return total;
-}
+
 
 const handOnDelete = (id) => {
     if (window.confirm("Are you sure, you want to delete the item?")) {
         taskList.splice(id, 1);
         display();
-        displayTotal();
     }
+}
+
+const createBarGraph = (arrData) => {
+    document.getElementById('bar-chart').innerHTML = '';
+    // Find the maximum value in the data array
+    const maxDataValue = Math.max(...arrData.map(obj => obj.data));
+
+    // Calculate the height of each bar based on the maximum value
+    const maxHeight = 100; // Max height of the bar graph
+
+    // Create the bar graph
+    const graphContainer = document.createElement('div');
+    graphContainer.classList.add('bar-graph');
+    graphContainer.classList.add('row');
+
+    arrData.forEach(obj => {
+        const bar = document.createElement('div');
+        bar.classList.add('bar');
+        bar.classList.add('col');
+        bar.classList.add('border');
+        bar.classList.add('shadow-lg');
+        bar.classList.add('mr-5');
+        const barHeight = (obj.data / maxDataValue) * maxHeight;
+        bar.style.height = `${barHeight}px`;
+        bar.textContent = obj.data;
+
+        const label = document.createElement('div');
+        label.classList.add('label');
+        label.textContent = obj.label;
+
+        bar.appendChild(label);
+        graphContainer.appendChild(bar);
+    });
+    document.getElementById('bar-chart').appendChild(graphContainer);
 }
 
 
